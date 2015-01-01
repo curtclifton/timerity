3dd1ee415e30c31e6b8192421b9fc6cb3bd6f37e
Curt Clifton <curt.clifton@mac.com>
2015-01-01 12:19:45 -0800

    Connected the name button. Implemented basic text input for naming a timer.
    Implemented delayed UI update on the TimerEditingInterfaceController, since we aren’t guaranteed of an order on when the controller becomes active versus when the text input callback is invoked.

--- Timerity/Timerity WatchKit Extension/TimerEditingInterfaceController.swift
+++ Timerity/Timerity WatchKit Extension/TimerEditingInterfaceController.swift
@@ -12,9 +12,12 @@ import WatchKit
 class TimerEditingInterfaceController: WKInterfaceController {
     private var timer: TimerInformation?
     private var isUnedited = true
-    
+    private var needsUpdate = true
+    private var isActive = true
+
     // outlets
     @IBOutlet var doneButton: WKInterfaceButton?
+    @IBOutlet var nameButton: WKInterfaceButton?
     
     override func awakeWithContext(context: AnyObject?) {
         if let timerID = context as? String {
@@ -24,28 +27,68 @@ class TimerEditingInterfaceController: WKInterfaceController {
                 } else {
                     self.timer = TimerInformation()
                 }
-                self._updateUserInterface()
+                self._update()
             }
         } else if context == nil {
             timer = TimerInformation()
-            _updateUserInterface()
+            _update()
         } else {
             assert(false, "Unexpected context \(context)")
         }
     }
     
+    override func willActivate() {
+        super.willActivate()
+        isActive = true
+        _updateIfNeeded()
+    }
+    
+    override func didDeactivate() {
+        isActive = false
+    }
+    
     //MARK: - Actions
+
+    @IBAction func nameButtonPressed() {
+        // CCC, 1/1/2015. if the timer is named, then include the name as a choice here
+        presentTextInputControllerWithSuggestions(["Tea", "Power Nap"], allowedInputMode: WKTextInputMode.Plain) { maybeInputText in
+            if let textInput = maybeInputText?.first as? String {
+                if !textInput.isEmpty {
+                    self.timer!.name = textInput
+                    self._update()
+                }
+            }
+        }
+    }
     
     @IBAction func doneButtonPressed() {
-        // CCC, 12/31/2014. need some mechanism for adding the row to the top-level listing
+        // CCC, 12/31/2014. need some mechanism for adding the row to the top-level listing. Probably give a way to register a timer-added callback on the database
         dismissController()
         timerDB.updateTimer(timer!)
     }
     
-    //Mark: - Private API
+    //MARK: - Private API
+
+    // CCC, 1/1/2015. Does it make sense to make a WKInterfaceController subclass or a helper object to handle the delayed update tap dance?
+    private func _setNeedsUpdate() {
+        needsUpdate = true
+    }
     
-    private func _updateUserInterface() {
+    private func _updateIfNeeded() {
+        if (!needsUpdate) {
+            return
+        }
         // CCC, 12/31/2014. implement
-//        doneButton!.setHidden(isUnedited)
+        nameButton!.setTitle(timer!.name)
+        doneButton!.setHidden(isUnedited)
+        needsUpdate = false
+    }
+    
+    /// If the interface is active, then updates the interface to match the state of the timer. Otherwise schedules an interface update for the next time the interface becomes active.
+    private func _update() {
+        _setNeedsUpdate()
+        if (isActive) {
+            _updateIfNeeded()
+        }
     }
 }
